e27e4ceae7318ffdff6b8416be7f09bf068741f9
Md Siddiqur Rahman <siddikcoder@gmail.com>
2023-04-04 00:19:30 +0600

    add new command

--- console.php
+++ console.php
@@ -3,9 +3,10 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Commands\ManifestCommand;
+use App\Commands\GenerateComponentCommand;
 use Symfony\Component\Console\Application;
 
 $app = new Application('Joomla 4 Extension Generator', 'v1.0.0');
-$app->add(new ManifestCommand());
+$app->add(new GenerateComponentCommand());
+$app->setDefaultCommand('generate:component', true);
 $app ->run();
